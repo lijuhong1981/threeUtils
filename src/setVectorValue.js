eba51292e1fff41f8da460eda62a90c57655e27c
lijuhong <lijuhong@pcitech.com>
2026-05-14 15:34:03 +0800

    Update setVectorValue.js

--- src/setVectorValue.js
+++ src/setVectorValue.js
@@ -2,8 +2,8 @@ import { Euler, Quaternion, Spherical, Vector2, Vector3, Vector4 } from "three";
 
 /**
  * 为threejs的Vector2或Vector3或Vector4或Euler或Quaternion或Spherical对象赋值
- * @param {Vector2|Vector3|Vector4|Euler|Quaternion|Spherical} vector
- * @param {Array<number>|object} value
+ * @param {Vector2|Vector3|Vector4|Euler|Quaternion|Spherical} vector - 要赋值的Vector2或Vector3或Vector4或Euler或Quaternion或Spherical对象实例
+ * @param {Array<number>|object} value - 向量值，可以是一个包含分量的数组（如[1, 2, 3]表示一个三维向量），或者另一个具有相同属性的对象实例（如{ x: 1, y: 2, z: 3 }表示一个三维向量）
  * @returns {Vector2|Vector3|Vector4|Euler|Quaternion|Spherical}
  */
 function setVectorValue(vector, value) {
